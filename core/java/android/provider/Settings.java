1725cb356ccde06d0107e596f8c23be032c31b14
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2017-01-11 16:04:17 -0800

    [1/2] frameworks: show charging current/voltage/battery info on lockscreen
    Thanx to ezio84 for the switch ;)
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    ezio 31/10/16: enabled by default
    
    also includes:
    
    frameworks_base: show battery info on lockscreen when charging
    
    - negociated max. charging current
    - negociated max. charging voltage
    - current battery temperature
    
    Change-Id: Ieb504739d451a304daf595074e42e8a042944ce6
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4179,6 +4179,12 @@ public final class Settings {
          */
         public static final String STATUSBAR_CLOCK_AM_PM_STYLE = "statusbar_clock_am_pm_style";
 
+          /**
+          * Whether to show the battery info on the lockscreen while charging
+          * @hide
+          */
+         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
         /**
          * Style of clock
          * 0 - Right Clock
