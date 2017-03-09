20d8d5ae5a3a60738868a520d1be92173c497cba
LorDClockaN <lordclockan@gmail.com>
2017-03-09 17:56:52 +0100

    FWB: Statusbar weather [1/2]
    OmniJaws rules!
    
    Change-Id: Id0ceb65dead6d224b2e13e8ca604de3b04df7737

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4439,6 +4439,17 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+        /**
+         * Status bar weather temperature
+         * 0: Hide the temperature
+         * 1: Display the temperature with scale and image
+         * 2: Display the temperature without scale and with image
+         * 3: Display the temparature with scale and without image
+         * 4: Display the temperature without scale and without image
+         * 5: Display the temperature image
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_WEATHER_TEMP = "status_bar_show_weather_temp";
 
         /**
          * Whether to show or hide the Settings Shortcut
