d6ceee392f5b4b9543fcd2db8a7a6a5d03e21acf
nasty007 <lazic.josip@gmail.com>
2017-09-16 17:21:09 +0200

    fix build
    Change-Id: I61ba21f1ca554d8dd7371eb0c59e57ab0d98419b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4680,7 +4680,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         mContext.unregisterReceiver(mBroadcastReceiver);
         mContext.unregisterReceiver(mDemoReceiver);
-        mContext.unregisterReceiver(mDUReceiver);
         mAssistManager.destroy();
 
         final SignalClusterView signalCluster =
